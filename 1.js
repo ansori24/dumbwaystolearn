f24a7b9fe326d47bb75480554c216612e0e98322
Misbah ansori <misbah.ansori24@gmail.com>
2020-01-18 22:18:29 +0700

    rounding uang

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@ const bunga = 0.025;
  
 for(i = 1; i <= 12; i++) {
    uang += uang * bunga;
-   console.log(`Uang pada bulan ${i} : ${uang}`)
+   console.log(`Uang pada bulan ${i} : ${Math.round(uang)}`)
 }
 
-console.log(`total uang selama 12 bulan adalah : ${uang}`)
+console.log(`total uang selama 12 bulan adalah : ${Math.round(uang)}`)
